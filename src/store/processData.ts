63250f7e50e9a57087f82fc9e48dca7d09529c38
Jean-Nicolas Veigel <art.jnveigel@gmail.com>
2023-01-14 19:11:36 +0100

    💬 process steps

--- src/store/processData.ts
+++ src/store/processData.ts
@@ -17,24 +17,12 @@ export const useProcessData = defineStore('processData', {
   state: (): State => ({
     cards: [
       {
-        title: 'Récupération du projet',
-        content:
-          'Neque voluptatem sapiente commodi tenetur minima autem totam occaecati. Tempore sint dolore illo eos sed qui aliquid reiciendis corporis. Rerum alias voluptatibus in optio aut. Quis ea autem reiciendis vero. Ex reprehenderit aperiam et. Neque voluptatem sapiente commodi tenetur minima autem totam occaecati.\n\nNeque voluptatem sapiente commodi tenetur minima autem totam occaecati. Tempore sint dolore illo eos sed qui aliquid reiciendis corporis.',
-        size: {},
-        animation: process1Animation,
-        index: 0,
-        translate: {
-          x: 0,
-          y: 0,
-        },
-      },
-      {
         title: 'Conformation des rushs',
         content:
           'Sed rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
         size: {},
         animation: process3Animation,
-        index: 1,
+        index: 0,
         translate: {
           x: 0,
           y: 0,
@@ -46,7 +34,7 @@ export const useProcessData = defineStore('processData', {
           'Voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo',
         size: {},
         animation: process2Animation,
-        index: 2,
+        index: 1,
         translate: {
           x: 0,
           y: 0,
@@ -58,7 +46,7 @@ export const useProcessData = defineStore('processData', {
           'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.',
         size: {},
         animation: process1Animation,
-        index: 3,
+        index: 2,
         translate: {
           x: 0,
           y: 0,
